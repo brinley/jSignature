064419620ed3e07bf8eea4ed5901ee2623c3f38c
Daniel Dotsenko <ddotsenko@willow-systems.com>
2011-10-13 17:22:39 -0700

    tweaking automation script for cache-busting

--- wscript.py
+++ wscript.py
@@ -2,6 +2,7 @@
 
 import shutil
 import os.path
+import time
 
 out = 'build'
 
@@ -12,21 +13,47 @@ def configure(conf):
     pass
 
 def build(bld):
-    
-    served_folder_prefix = "/bin/nginx/html/jsig/"
-    
+
+    served_folder_prefix = "./release/"
     todeploy = [
         ["js/libs/jquery-1.6.2.min.js", "js/libs/jquery.js"]
-        , ["js/business_logic.js", "js/business_logic.js"]
-        , ["index.html", "index.html"]
-        , ["jSignature.js", "js/libs/jquery.jSignature.js"]
-        
+        , ["js/business_logic.js", r"js/business_logic.js", 'cache busting']
+        , ["jSignature.js", r"js/libs/jquery.jSignature.js", 'cache busting']
+        , ["index.html", "index.html"] # item immediately following cache busting item(s) gets edited for all the busted names.
         ]
-    
+
+    cache_busting_string = ".%s" % int(time.time())
+    cache_busting_list = []
+    cache_busting_setup_ready = False
+
+    # moving files in place
+    if os.path.exists(served_folder_prefix):
+        for node in os.listdir(served_folder_prefix):
+            shutil.rmtree(served_folder_prefix + node, True)
     for item in todeploy:
         src = item[0]
-        trg = served_folder_prefix + item[1]
-        trg_folder, trg_file = os.path.split(trg)
+        trg = item[1]
+        trg_full = served_folder_prefix + trg 
+        trg_folder, trg_file = os.path.split(trg_full)
         if not os.path.exists(trg_folder):
             os.makedirs(trg_folder)
-        shutil.copy(src, trg)
+        shutil.copy(src, trg_full)
+        
+        # this is cache_busting
+        if len(item)>2 and item[2] == 'cache busting':
+            trgstart, trgend = os.path.splitext(trg_full)
+            trg_busted = trgstart + cache_busting_string + trgend
+            shutil.move(trg_full, trg_busted)
+            cache_busting_list.append( trg )
+            cache_busting_setup_ready = True
+        elif cache_busting_setup_ready:
+            content = open(trg_full).read()
+            for filename in cache_busting_list:
+                trgstart, trgend = os.path.splitext(filename)
+                trg_busted = trgstart + cache_busting_string + trgend
+                content = content.replace(filename, trgstart + cache_busting_string + trgend)
+            open(trg_full, 'w').write(content + '<!-- autoedited for cache busting -->')
+            cache_busting_list = []
+
+if __name__ == '__main__':
+    print("This is a Wak build automation tool script. Please, get Wak on GitHub and run it against the folder containing this automation script.")
