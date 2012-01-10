699e81e7f83b33b48cc5e19513ef8900c7744ef4
Daniel Dotsenko <ddotsenko@willow-systems.com>
2012-01-10 12:50:14 -0800

    Adjusting, simplifying build script for moved files.

--- wscript.py
+++ wscript.py
@@ -16,45 +16,41 @@ def build(bld):
 
     served_folder_prefix = "./release/"
     todeploy = [
-        ["libs/jquery.js", "js/libs/jquery.js"]
-        , ["libs/flashcanvas.js", "js/libs/flashcanvas.js"]
-        , ["libs/flashcanvas.swf", "js/libs/flashcanvas.swf"]
-        , ["jSignature.js", "js/libs/jquery.jSignature.js", 'cache busting']
-        , ["index.html", "index.html"] # item immediately following cache busting item(s) gets edited for all the busted names.
+        ['jq', "libs/jquery.js", "js/libs/jquery.js"]
+        , ['fcj', "libs/flashcanvas.js", "js/libs/flashcanvas.js"]
+        , ['fcswf', "libs/flashcanvas.swf", "js/libs/flashcanvas.swf"]
+        , ['jsig', "jSignature.js", "js/libs/jquery.jSignature.${CACHEBUST}.js"]
+        , ['index', "index.html", "index.html", ['jq','fcj','jsig']] # item immediately following cache busting item(s) gets edited for all the busted names.
     ]
-
-    cache_busting_string = ".%s" % int(time.time())
-    cache_busting_list = []
-    cache_busting_setup_ready = False
-
-    # moving files in place
+    todeploy_map = {} # used for string replacement in HTML files. See list above next to index.html
+  
+    #destination folder
     if os.path.exists(served_folder_prefix):
         for node in os.listdir(served_folder_prefix):
             shutil.rmtree(served_folder_prefix + node, True)
+
+    # deploying files
+    cache_busting_string = "%s" % int(time.time())
     for item in todeploy:
-        src = item[0]
-        trg = item[1]
+        name = item[0]
+        src = item[1]
+        trg = item[2].replace("${CACHEBUST}", cache_busting_string)
+        todeploy_map[name] = {'src':src, 'trg':trg}
+
         trg_full = served_folder_prefix + trg 
         trg_folder, trg_file = os.path.split(trg_full)
         if not os.path.exists(trg_folder):
             os.makedirs(trg_folder)
         shutil.copy(src, trg_full)
-        
-        # this is cache_busting
-        if len(item)>2 and item[2] == 'cache busting':
-            trgstart, trgend = os.path.splitext(trg_full)
-            trg_busted = trgstart + cache_busting_string + trgend
-            shutil.move(trg_full, trg_busted)
-            cache_busting_list.append( trg )
-            cache_busting_setup_ready = True
-        elif cache_busting_setup_ready:
+
+        # editing text file for changes to file names.
+        # inly when we explicitely give nicknames of the filenames we need to replace.
+        if len(item) > 3:
             content = open(trg_full).read()
-            for filename in cache_busting_list:
-                trgstart, trgend = os.path.splitext(filename)
-                trg_busted = trgstart + cache_busting_string + trgend
-                content = content.replace(filename, trgstart + cache_busting_string + trgend)
-            open(trg_full, 'w').write(content + '<!-- autoedited for cache busting -->')
-            cache_busting_list = []
+            # for each file nickname
+            for nick in item[3]:
+                content = content.replace(todeploy_map[nick]['src'], todeploy_map[nick]['trg'])
+            open(trg_full, 'w').write(content + '<!-- autoedited for deployment -->')
 
 if __name__ == '__main__':
     print("This is a Wak build automation tool script. Please, get Wak on GitHub and run it against the folder containing this automation script.")
