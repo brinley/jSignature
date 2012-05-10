ab8bd02a58c7c72eb47079cff2e750e735d6e851
Daniel Dotsenko <ddotsenko@willow-systems.com>
2012-05-10 11:24:58 -0700

    Core: We are growing evented more and more. Adding our own PubSub. it's tiny and good.

--- jSignature.js
+++ jSignature.js
@@ -879,6 +879,76 @@ var Initializer = function($){
 		return this
 	}
 
+	var PubSubInstance = new (function(){
+	    'use strict'
+	    /*  @preserve 
+	    -----------------------------------------------------------------------------------------------
+	    PubSub
+	    2012 (c) ddotsenko@willowsystems.com
+	    based on Peter Higgins (dante@dojotoolkit.org)
+	    Loosely based on Dojo publish/subscribe API, limited in scope. Rewritten blindly.
+	    Original is (c) Dojo Foundation 2004-2010. Released under either AFL or new BSD, see:
+	    http://dojofoundation.org/license for more information.
+	    -----------------------------------------------------------------------------------------------
+	    */
+	    this.topics = {};
+	    /**
+	     * Allows caller to emit an event and pass arguments to event listeners.
+	     * @public
+	     * @function
+	     * @param topic {String} Name of the channel on which to voice this event
+	     * @param **arguments Any number of arguments you want to pass to the listeners of this event.
+	     */
+	    this.publish = function(topic, arg1, arg2, etc) {
+	        'use strict'
+	        if (this.topics[topic]) {
+	            var currentTopic = this.topics[topic]
+	            , args = Array.prototype.slice.call(arguments, 1)
+	            
+	            for (var i = 0, l = currentTopic.length; i < l; i++) {
+	                currentTopic[i].apply(null, args);
+	            }
+	        }
+	    };
+	    /**
+	     * Allows listener code to subscribe to channel and be called when data is available 
+	     * @public
+	     * @function
+	     * @param topic {String} Name of the channel on which to voice this event
+	     * @param callback {Function} Executable (function pointer) that will be ran when event is voiced on this channel.
+	     * @returns {Object} A token object that cen be used for unsubscribing.  
+	     */
+	    this.subscribe = function(topic, callback) {
+	        'use strict'
+	        if (!this.topics[topic]) {
+	            this.topics[topic] = [callback];
+	        } else {
+	            this.topics[topic].push(callback);
+	        }
+	        return {
+	            "topic": topic,
+	            "callback": callback
+	        };
+	    };
+	    /**
+	     * Allows listener code to unsubscribe from a channel 
+	     * @public
+	     * @function
+	     * @param token {Object} A token object that was returned by `subscribe` method 
+	     */
+	    this.unsubscribe = function(token) {
+	        if (this.topics[token.topic]) {
+	            var currentTopic = this.topics[token.topic]
+	            
+	            for (var i = 0, l = currentTopic.length; i < l; i++) {
+	                if (currentTopic[i] === token.callback) {
+	                    currentTopic.splice(i, 1)
+	                }
+	            }
+	        }
+	    }
+	})();
+	
 	//These are exposed as methods under $obj.jSignature('methodname', *args)
 	var methods = {
 		'init' : function( options ) {
@@ -922,6 +992,7 @@ var Initializer = function($){
 		, 'importData' : _setDrawingData
 		// was mistakenly introduced instead of 'importData' in v2
 		, 'setData' : _setDrawingData
+		, 'PubSub' : function(){return PubSubInstance} 
 	} // end of methods declaration.
 	
 	$.fn[apinamespace] = function(method) {
