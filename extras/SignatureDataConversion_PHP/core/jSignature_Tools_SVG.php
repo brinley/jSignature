1aeff2431f1c60154856445fbff899f35b86297c
Daniel Dotsenko <ddotsenko@willow-systems.com>
2012-06-21 12:09:06 -0700

    PHP extras: ported SVG renderer from JavaScript

--- extras/SignatureDataConversion_PHP/core/jSignature_Tools_SVG.php
+++ extras/SignatureDataConversion_PHP/core/jSignature_Tools_SVG.php
@@ -1,102 +1,110 @@
 <?php
-
 /** @license
 jSignature v2 SVG export plugin.
 Copyright (c) 2012 Willow Systems Corp http://willow-systems.com
 MIT License <http://www.opensource.org/licenses/mit-license.php>
 */
-	
-	function round (number, position){
-		var tmp = Math.pow(10, position)
-		return Math.round( number * tmp ) / tmp
-	}
+class jSignature_Tools_SVG {
+
 	/**
 	 * This is a simple, points-to-lines (not curves) renderer. 
 	 * Keeping it around so we can activate it from time to time and see
 	 * if smoothing logic is off much.
 	 * @public
 	 * @function
-	 * @returns {String} Like so "l 1 2 3 5' with stroke as long line chain. 
+	 * @param $stroke {Array} Hash representing a single stroke, with two properties 
+	 * 		('x' => array(), 'y' => array()) where 'array()' is an array of 
+	 *		coordinates for that axis.
+	 * @returns {String} Like so 'M 53 7 l 1 2 3 4 -5 -6 5 -6' which is in format of SVG's Path.d argument.
 	 */
-	function addstroke(stroke, shiftx, shifty){
-		// we combine strokes data into string like this:
-		// 'M 53 7 l 1 2 3 4 -5 -6 5 -6'
-		// see SVG documentation for Path element's 'd' argument.
-		var lastx = stroke.x[0]
-		, lasty = stroke.y[0]
-		, i
-		, l = stroke.x.length
-		, answer = ['M', round( lastx - shiftx, 2) , round( lasty - shifty, 2), 'l']
+	private function addstroke($stroke, $shiftx, $shifty){
+		$lastx = $stroke['x'][0];
+		$lasty = $stroke['y'][0];
+		$i;
+		$l = sizeof( $stroke['x'] );
+		$answer = array('M', round( $lastx - $shiftx, 2) , round( $lasty - $shifty, 2), 'l');
 		
-		if (l === 1){
+		if ($l == 1){
 			// meaning this was just a DOT, not a stroke.
-			// instead of creating a circle, we just create a short line
-			answer.concat(1, -1)
+			// instead of creating a circle, we just create a short line "up and to the right" :)
+			array_push($answer, 1);
+			array_push($answer, -1);
 		} else {
-			for(i = 1; i < l; i++){
-				answer = answer.concat(stroke.x[i] - lastx, stroke.y[i] - lasty)
-				lastx = stroke.x[i]
-				lasty = stroke.y[i]
+			for($i = 1; $i < $l; $i++){
+				array_push( $answer, $stroke['x'][$i] - $lastx);
+				array_push( $answer, $stroke['y'][$i] - $lasty);
+				$lastx = $stroke['x'][$i];
+				$lasty = $stroke['y'][$i];
 			}
 		}
-		return answer.join(' ')
+		return implode(' ', $answer);
 	} 
 	
-	function compressstrokes(data){
-		'use strict'
-		var answer = [
+	public function NativeToSVG($data){
+		$answer = array(
 			'<?xml version="1.0" encoding="UTF-8" standalone="no"?>'
 			, '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">'
-        ]
-		, i , l = data.length
-		, stroke
-		, xlimits = []
-		, ylimits = []
-		, sizex = 0
-		, sizey = 0
-		, shiftx = 0
-		, shifty = 0
-		, minx, maxx, miny, maxy, padding = 1
+        );
+		$i;
+		$l = sizeof( $data );
+		$stroke;
+		$xlimits = array();
+		$ylimits = array();
+		$sizex = 0;
+		$sizey = 0;
+		$shiftx = 0;
+		$shifty = 0;
+		$minx;
+		$maxx;
+		$miny;
+		$maxy;
+		$padding = 1;
 		
-		if(l !== 0){
-			for(i = 0; i < l; i++){
-				stroke = data[i]
-				xlimits = xlimits.concat(stroke.x)
-				ylimits = ylimits.concat(stroke.y)
+		if($l !== 0){
+			for($i = 0; $i < $l; $i++){
+				$stroke = $data[$i];
+				$xlimits = array_merge($xlimits, $stroke['x']);
+				$ylimits = array_merge($ylimits, $stroke['y']);
 			}
-			 
-			minx = Math.min.apply(null, xlimits) - padding
-			maxx = Math.max.apply(null, xlimits) + padding
-			miny = Math.min.apply(null, ylimits) - padding
-			maxy = Math.max.apply(null, ylimits) + padding
-			shiftx = minx < 0? 0 : minx
-			shifty = miny < 0? 0 : miny
-			sizex = maxx - minx
-			sizey = maxy - miny
+
+			$minx = min($xlimits) - $padding;
+			$maxx = max($xlimits) + $padding;
+			$miny = min($ylimits) - $padding;
+			$maxy = max($ylimits) + $padding;
+			$shiftx = $minx < 0 ? 0 : $minx;
+			$shifty = $miny < 0 ? 0 : $miny;
+			$sizex = $maxx - $minx;
+			$sizey = $maxy - $miny;
 		}
 		
-		answer.push(
-			'<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="'+ 
-			sizex.toString() +
-			'" height="'+ 
-			sizey.toString() +
+		array_push( $answer, 
+			'<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="'.
+			$sizex.
+			'" height="'.
+			$sizey.
 			'">'
-		)
+		);
 		
 //		// This is a nice idea: use style declaration on top, and mark the lines with 'class="f"'
 //		// thus saving space in svg... 
-//		// alas, many SVG renderers don't understand "class" and render the strokes in default "fill = black, no stroke" style. Ugh!!!
-//		// TODO: Rewrite ImageMagic / GraphicsMagic, InkScape, http://svg.codeplex.com/ to support style + class. until then, we hardcode the stroke style within the path. 
-//		answer.push(
-//			'<style type="text/css"><![CDATA[.f {fill:none;stroke:#000000;stroke-width:2;stroke-linecap:round;stroke-linejoin:round}]]></style>'
+//		// alas, many SVG renderers don't understand "class" and render the $strokes in default "fill = black, no $stroke" style. Ugh!!!
+//		// TODO: Rewrite ImageMagic / GraphicsMagic, InkScape, http://svg.codeplex.com/ to support style + class. until then
+//		// , we hardcode the stroke style within the path. 
+//		$answer.push(
+//			'<style type="text/css"><![C$data[.f {fill:none;$stroke:#000000;$stroke-width:2;$stroke-linecap:round;$stroke-linejoin:round}]]></style>'
 //		)
 
-		for(i = 0; i < l; i++){
-			answer.push('<path fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" d="'+ addstroke(data[i], shiftx, shifty) +'"/>')
+		for($i = 0; $i < $l; $i++){
+			array_push( 
+				$answer
+				, '<path fill="none" stroke="#000000" stroke-width="2"'.
+				  ' stroke-linecap="round" stroke-linejoin="round" d="'.
+				  $this->addstroke($data[$i], $shiftx, $shifty) . '"/>'
+			);
 		}
 
-		answer.push('</svg>')
-		return answer.join('')
+		array_push($answer, '</svg>');
+		return implode('', $answer);
 	}
-
+}
 ?>
--- extras/SignatureDataConversion_PHP/tests/test_jSignature_Tools.php
+++ extras/SignatureDataConversion_PHP/tests/test_jSignature_Tools.php
@@ -1,5 +1,6 @@
 <?php
 require_once '../core/jSignature_Tools_Base30.php';
+require_once '../core/jSignature_Tools_SVG.php';
 
 class jSignature_Tools_Tests extends PHPUnit_Framework_TestCase
 {
@@ -40,6 +41,32 @@ class jSignature_Tools_Tests extends PHPUnit_Framework_TestCase
             $expected
             , $actual
         );
+    }
+
+    public function testNative_to_SVG() {
+
+        $converter = new jSignature_Tools_SVG();
+
+        // $base30 = "3E13Z5Y5_1O24Z66_1O1Z3_3E2Z4";
+        $native = array(
+            array(
+                'x'=>array(100,101,104,99,104)
+                ,'y'=>array(50,52,56,50,44)
+            )
+            ,array(
+                'x'=>array(50,51,48)
+                ,'y'=>array(100,102,98)
+            )
+        );
+
+        $expected = '<?xml version="1.0" encoding="UTF-8" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="58" height="60"><path fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" d="M 53 7 l 1 2 3 4 -5 -6 5 -6"/><path fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" d="M 3 57 l 1 2 -3 -4"/></svg>';
+        $actual = $converter->NativeToSVG($native);
+
+        $this->assertEquals(
+            $expected
+            , $actual
+        );
+
     }
 
 }
